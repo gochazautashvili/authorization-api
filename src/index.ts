b2f6874877323e1ccaabc2af1ecde83c23e5fdbb
Gocha <gochazautashvili2017@gmail.com>
2024-08-07 10:59:36 +0400

    new

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,12 @@ import "./middleware/facebook-auth";
 
 const app: Express = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
